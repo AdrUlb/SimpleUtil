2f8b21fd322e7f69ab283c586ec55ec654bf0daa
Adrian <adrian.ulbrich.2003@gmail.com>
2023-08-30 12:15:31 +0200

    Implement Logger

---
+++ Source/SimpleUtil/Logger.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace SimpleUtil;
+
+public sealed class Logger
+{
+    private readonly bool logToConsole;
+    private readonly string? logFilePath;
+
+    public Logger(bool logToConsole = true, string? logFilePath = null)
+    {
+        this.logToConsole = logToConsole;
+        this.logFilePath = logFilePath;
+    }
+
+    private void LogToConsole(string message)
+    {
+        if (logToConsole)
+            Console.WriteLine(message);
+    }
+
+    private void LogToFile(string message)
+    {
+        if (logFilePath != null)
+            File.AppendAllText(logFilePath, $"{message}\n");
+    }
+
+    public void Log(string message)
+    {
+        LogToConsole(message);
+        LogToFile(message);
+    }
+}
